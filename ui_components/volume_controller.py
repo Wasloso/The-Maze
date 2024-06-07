da8b2eeff5bb3eb1c3aaaa644fe8bbc316cd8579
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2024-06-07 22:41:09 +0200

    fixed

--- ui_components/volume_controller.py
+++ ui_components/volume_controller.py
@@ -8,7 +8,7 @@ from .ui_component import UIComponent
 class VolumeController(UIComponent):
     def __init__(self, position, desiredSize) -> None:
         image = Surface(desiredSize, pygame.SRCALPHA)
-        super().__init__(position, image.convert_alpha(), desiredSize)
+        super().__init__(position, desiredSize, image.convert_alpha())
 
     def draw(self, screen: Surface) -> None:
-        pass
+        super().draw(screen)
