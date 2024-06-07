b9fb37c008bf807f10a355c0ac5b46d42f8857a4
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2024-06-07 22:41:48 +0200

    added non functional VolumeController

--- screens/options_screen.py
+++ screens/options_screen.py
@@ -1,16 +1,25 @@
-from .screen_base import ScreenBase
-from pygame import *
+from .screen_base import *
+import pygame
+from pygame.surface import Surface
+import sys
+
+sys.path.append("../")
+from ui_components.volume_controller import VolumeController
 
 
 class OptionsScreen(ScreenBase):
     def __init__(self, title: str, width: int, height: int) -> None:
-        super().__init__(title, width, height)
+        super().__init__(title, width, height, screenName=OPTIONS)
+        self.volumeController = VolumeController((100, 0), (100, 100))
+        self.components = pygame.sprite.Group(self.volumeController)
 
     def draw(self, screen: Surface) -> None:
-        pass
+        self.components.draw(screen)
+        super().draw(screen)
 
-    def update(self, events: list) -> None:
-        pass
+    def update(self, events: list, keys) -> None:
+        self.components.update(events)
+        super().update(events, keys)
 
     def makeCurrent(self) -> None:
         pass
