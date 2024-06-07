de95d9d3b88cf25fc3bd436d7f8eb90983243edc
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2024-06-07 18:02:10 +0200

    added docstring and background image

--- screens/screen_base.py
+++ screens/screen_base.py
@@ -1,5 +1,6 @@
 import pygame
 from abc import ABC, abstractmethod
+from .button import Button
 
 MAIN_MENU = "main_menu_screen"
 PLAY = "play_screen"
@@ -8,16 +9,39 @@ GAME = "game_screen"
 
 
 class ScreenBase(ABC):
-    def __init__(self, title: str, width: int, height: int) -> None:
+    def __init__(
+        self,
+        title: str,
+        width: int,
+        height: int,
+        previous=MAIN_MENU,
+        backgroundImage: pygame.Surface = None,
+    ) -> None:
+        """
+        Screen class which is base for all screens in the app.
+        :param title: Title of the screen
+        :param width: Width of the screen
+        :param height: Height of the screen
+        :param previous: Name of the previous screen
+        :param backgroundImage: Background image of the screen, if None, screen will be black
+        """
         self.height: int = height
         self.width: int = width
         self.title: str = title
         self.done: bool = False
         self.next_screen: str = None
+        self.previous = previous
+        if backgroundImage:
+            self.backgroundImage = pygame.transform.scale(
+                backgroundImage, (self.width, self.height)
+            )
+        else:
+            self.backgroundImage = pygame.Surface((self.width, self.height))
+            self.backgroundImage.fill((0, 0, 0))
 
     @abstractmethod
     def draw(self, screen: pygame.Surface) -> None:
-        screen.fill((0, 0, 0))
+        screen.blit(self.backgroundImage, (0, 0))
 
     @abstractmethod
     def update(self, events: list, keys) -> None:
@@ -25,7 +49,7 @@ class ScreenBase(ABC):
 
     @abstractmethod
     def makeCurrent(self) -> None:
-        pygame.display.set_caption(self.title)
+        # to moze mozna lepiej zrobic, w ogole cale zmienianie ekranow jest mozliwe do poprawienia xd
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.done = False
         self.next_screen = None
