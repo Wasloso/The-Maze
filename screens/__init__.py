dee511efb4b732ebc368ccfe979bcda4cc0f9f2f
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2024-06-08 18:31:14 +0200

    added credits screen to export

--- screens/__init__.py
+++ screens/__init__.py
@@ -3,3 +3,4 @@ from .main_menu import MainMenu
 from .options_screen import OptionsScreen
 from .play_screen import PlayScreen
 from .game_screen import GameScreen
+from .credits_screen import CreditsScreen
