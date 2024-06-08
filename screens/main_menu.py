91a971d23741a3a4611b2dfd8c34567a5ef2bc2a
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2024-06-08 13:25:18 +0200

    now uses assetsloader

--- screens/main_menu.py
+++ screens/main_menu.py
@@ -6,40 +6,46 @@ import sys
 sys.path.append("../")
 from ui_components.button import Button
 from ui_components.volume_controller import VolumeController
+from assets.assets_loader import AssetsLoader
 
 
 class MainMenu(ScreenBase):
     def __init__(self, title: str, width: int, height: int) -> None:
         super().__init__(title, width, height, screenName=MAIN_MENU)
-        play_image = pygame.image.load("assets/buttons/button_play.png")
         alt = pygame.surface.Surface((200, 100))
         alt.fill((255, 0, 0))
         self.play_button = Button(
-            image=play_image,
-            position=(self.width // 2, self.height // 2 - 200),
+            image=AssetsLoader.get_button("play_button"),
+            position=(self.width // 2, self.height // 2 - 50),
             callback=self.start_game,
         )
-        options_image = pygame.image.load("assets/buttons/placeholder.png")
-        self.options_button = Button(
-            image=options_image,
-            position=(self.width // 2, self.height // 2),
+        self.settings_button = Button(
+            image=AssetsLoader.get_button("setting_button"),
+            position=(self.width // 2, self.height // 2 + 100),
             callback=self.open_options,
             altImage=alt,
         )
-        quit_image = pygame.image.load("assets/buttons/placeholder.png")
-        self.quit_button = Button(
-            image=quit_image,
-            position=(self.width // 2, self.height // 2 + 200),
-            callback=self.start_game,
+        self.exit_button = Button(
+            image=AssetsLoader.get_button("exit_button"),
+            position=(150, self.height - 100),
+            altImage=alt,
+        )
+        self.credits_button = Button(
+            image=AssetsLoader.get_button("credits_button"),
+            position=(self.width - 150, self.height - 100),
             altImage=alt,
         )
         self.buttons: Group[Button] = pygame.sprite.Group(
-            self.play_button, self.options_button, self.quit_button
+            self.play_button,
+            self.settings_button,
+            self.exit_button,
+            self.credits_button,
         )
         self.selected_maze = None
 
     def draw(self, screen: pygame.Surface) -> None:
         super().draw(screen)
+        # TODO: z jakiegos powodu uzywanie self.buttons.draw sprawia ze one sie nie aktualizuja (readme)
         for button in self.buttons:
             button.draw(screen)
 
