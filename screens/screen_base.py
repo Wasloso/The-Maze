95377a6ea28340966e6b3440ebee4608f32e0b54
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2024-06-11 22:44:45 +0200

    added background field to avoid creating new background image everytime

--- screens/screen_base.py
+++ screens/screen_base.py
@@ -10,7 +10,7 @@ from ui_components.ui_component import UIComponent
 
 # TODO maybe it can be moved somewhere else eg. constants.py so it can be used everywhere (assets manager)
 MAIN_MENU = "main_menu_screen"
-OPTIONS = "options_screen"
+SETTINGS = "settings_screen"
 GAME = "game_screen"
 CREDITS = "credits_screen"
 MAZE_SELECTION = "maze_selection_screen"
@@ -26,12 +26,17 @@ class ScreenBase(ABC):
         self.previous_screen = previous_screen  # TODO: we can get rid of this i guess
         self.screen_name = screen_name
         self.manager = manager
+        self.background = AssetsLoader.get_background(self.screen_name)
+        self.background = UIComponent(
+            (0, 0), self.background.get_size(), self.background
+        )
 
     def draw(self, surface: pygame.Surface) -> None:
-        UIComponent(
-            (0, 0), surface.get_size(), AssetsLoader.get_background(self.screen_name)
-        ).draw(surface)
-        # FIXME: it should be moved to the constructor: background
+        if self.background.rect.size != surface.get_size():
+            self.background = UIComponent(
+                (0, 0), surface.get_size(), self.background.image
+            )
+        self.background.draw(surface)
 
     @abstractmethod
     def update(self, events: list, keys) -> None:
