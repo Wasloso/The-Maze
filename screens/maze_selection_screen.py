38f4f3ef74a0eb77701c0438e5f7d6d9e949d718
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2024-06-12 15:07:10 +0200

    added loading mazes here

--- screens/maze_selection_screen.py
+++ screens/maze_selection_screen.py
@@ -2,6 +2,7 @@ from .screen_base import *
 from pygame.surface import Surface
 from pygame import Surface
 from ui_components.button import Button
+from data.mazes_manager import MazesManager
 
 
 class MazeSelectionScreen(ScreenBase):
@@ -10,12 +11,32 @@ class MazeSelectionScreen(ScreenBase):
         self.back_button = Button.go_back_button(
             position=(50, 50), callback=lambda: self.manager.back(previous_screen)
         )
+        self.buttons: list[Button] = []
+        self.mazes = MazesManager().load_mazes()
+        for maze in self.mazes:
+            self.buttons.append(
+                Button(
+                    image=AssetsLoader.get_button("play_button"),
+                    altImage=AssetsLoader.get_button("play_button", hovered=True),
+                    desiredSize=(300, 100),
+                    callback=lambda m=maze: self.manager.start_game(
+                        self, self.manager, m
+                    ),
+                )
+            )
 
     def draw(self, surface: Surface) -> None:
         super().draw(surface)
         self.back_button.draw(surface)
+        for i, button in enumerate(self.buttons):
+            button.set_position(
+                (surface.get_width() // 2, surface.get_height() // 2 + i * 150)
+            )
+            button.draw(surface)
 
     def update(self, events, keys) -> None:
         super().update(events, keys)
         for event in events:
             self.back_button.update(event)
+            for button in self.buttons:
+                button.update(event)
