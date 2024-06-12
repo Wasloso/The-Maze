cd0f6b0884f1085adb1f11d91cb5a633c6ffb43e
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2024-06-12 17:03:41 +0200

    added type hints, added WSAD movement

--- screens/game_screen.py
+++ screens/game_screen.py
@@ -1,5 +1,7 @@
+from pygame import Rect
 from pygame.surface import Surface
 
+from maze.cell import Cell
 from maze.maze import Maze
 from maze.objective import Objective
 from maze.player import Player
@@ -12,45 +14,47 @@ class GameScreen(ScreenBase):
     def __init__(
         self,
         previous_screen: Optional[ScreenBase],
-        manager,
+        manager,  # FIXME: add type hint here (now gives an Importerror)
         maze: Maze,
         ai: bool = False,
     ) -> None:
         super().__init__(previous_screen, manager, GAME)
-        self.maze = maze
-        self.ai = ai
-
-        # player_img = pygame.image.load("assets/player.png")
-        playerImg = AssetsLoader.get_player("idle" if not ai else "idle_ai")
-        self.player = Player(
-            self.maze.player_start, 2, size=self.maze.cell_size // 3, image=playerImg
+        self.maze: Maze = maze
+        self.ai: bool = ai
+        playerImg: Surface = AssetsLoader.get_player("idle" if not ai else "idle_ai")
+        self.player: Player = Player(
+            start_position=self.maze.player_start,
+            velocity=2,
+            size=self.maze.cell_size // 3,
+            image=playerImg,
         )
-        self.objective = Objective(
-            self.maze.objective_position, size=self.maze.cell_size // 2
+        self.objective: Objective = Objective(
+            position=self.maze.objective_position, size=self.maze.cell_size // 2
         )
-        self.back_button = Button.go_back_button(
+        self.back_button: Button = Button.go_back_button(
             position=(25, 25),
             desiredSize=(50, 50),
             callback=lambda: self.manager.back(previous_screen),
-            # TODO: change to play screen when implemented
         )
-        self.cells = [cell for row in self.maze.grid for cell in row]
-        self.collidable_cells = [cell.rect for cell in self.cells if cell.collidable]
-        self.wallImage = AssetsLoader.get_cell("wall")
-        self.floorImage = AssetsLoader.get_cell("floor")
+        self.cells: list[Cell] = [cell for row in self.maze.grid for cell in row]
+        self.collidable_cells: list[Cell] = [
+            cell.rect for cell in self.cells if cell.collidable
+        ]
+        self.wallImage: Surface = AssetsLoader.get_cell("wall")
+        self.floorImage: Surface = AssetsLoader.get_cell("floor")
 
     def draw(self, surface: Surface) -> None:
-        # super().draw(screen) - renderowanie tla bardzo obniza fps
+        super().draw(surface)
         for cell in self.cells:
             if cell.collidable:
-                surface.blit(self.wallImage, cell.rect)
+                surface.blit(self.wallImage, cell.rect.topleft)
             else:
-                surface.blit(self.floorImage, cell.rect)
+                surface.blit(self.floorImage, cell.rect.topleft)
         self.player.draw(surface)
         self.objective.draw(surface)
         self.back_button.draw(surface)
 
-    def update(self, events: list, keys) -> None:
+    def update(self, events: list, keys: list) -> None:
         for event in events:
             self.back_button.update(event)
         self.handle_buttons_click(keys)
@@ -58,18 +62,21 @@ class GameScreen(ScreenBase):
             # TODO: Implement win screen
             pass
 
-    def handle_buttons_click(self, keys):
-        multiplier = 2 if keys[pygame.K_LSHIFT] else 1
-        if keys[pygame.K_UP]:
+    def handle_buttons_click(self, keys) -> None:
+        multiplier: int = 2 if keys[pygame.K_LSHIFT] else 1
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
             self.move_player(pygame.K_UP, multiplier)
-        if keys[pygame.K_DOWN]:
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
             self.move_player(pygame.K_DOWN, multiplier)
-        if keys[pygame.K_LEFT]:
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             self.move_player(pygame.K_LEFT, multiplier)
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             self.move_player(pygame.K_RIGHT, multiplier)
 
-    def move_player(self, direction, multiplier=1):
-        new_rect = self.player.try_move(direction, multiplier)
+    def move_player(self, direction, multiplier=1) -> None:
+        new_rect: Rect = self.player.try_move(direction, multiplier)
         if new_rect.collidelist(self.collidable_cells) == -1:
             self.player.rect = new_rect
+
+
+1
