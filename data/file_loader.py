54710362f01a735cd49a974c83d65244bb2f7b61
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2024-06-12 17:34:38 +0200

    added type hints

--- data/file_loader.py
+++ data/file_loader.py
@@ -2,17 +2,16 @@ import json
 
 
 class FileManager:
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, path: str) -> None:
+        self.path: str = path
 
     def load(self) -> dict:
         with open(self.path) as f:
             return json.load(f)
 
-    def save(self, data: dict):
+    def save(self, data: dict) -> None:
         with open(self.path, "w") as f:
             # TODO: add better formatting
             data: str = json.dumps(data, indent=4)
             data.replace("    ", "\t")
-
             f.write(data)
