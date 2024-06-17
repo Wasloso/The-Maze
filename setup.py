368e8f02d4c32b3092025762cc6ca0f2484933cf
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2024-06-17 21:09:32 +0200

    added manifest.in

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='themaze',
-    version='0.0.1',
-    description='A(maze)ing game TheMaze',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='Partyk Łuszczek & Tymoteusz Lango',
-    url='https://github.com/Wasloso/TheMaze',
+    name="themaze",
+    version="0.0.1",
+    description="A(maze)ing game TheMaze",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Partyk Łuszczek & Tymoteusz Lango",
+    url="https://github.com/Wasloso/TheMaze",
     packages=find_packages(),
-    include_package_data=True,
-    install_requires=['pygame>=2.0.0'],
+    install_requires=["pygame>=2.0.0"],
     entry_points={
-        'console_scripts': [
-            'themaze=app:main',
+        "console_scripts": [
+            "themaze=app:main",
         ],
     },
+    requires=["pygame"],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
