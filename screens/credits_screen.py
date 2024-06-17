4da1791735495292c4c43dd42132f5524d34e53a
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2024-06-17 16:32:00 +0200

    added text to credits screen

--- screens/credits_screen.py
+++ screens/credits_screen.py
@@ -2,6 +2,7 @@ from .screen_base import *
 import pygame
 from pygame.sprite import Group
 import sys
+from pygame.surface import Surface
 
 sys.path.append("../")
 from ui_components.button import Button
@@ -15,10 +16,26 @@ class CreditsScreen(ScreenBase):
         self.back_button = Button.go_back_button(
             position=(50, 50), callback=lambda: self.manager.back(previous_screen)
         )
+        self.text = pygame.transform.scale(
+            AssetsLoader.get_button("selection_button"), (600, 400)
+        )
+        UIComponent.add_text_to_surface(
+            text=["Made by", "Tymoteusz Lango", "&", "Patryk Łuszczek"],
+            surface=self.text,
+            font=pygame.font.Font(None, 80),
+            text_color=(0, 0, 0),
+        )
 
     def draw(self, surface: pygame.Surface) -> None:
         super().draw(surface)
         self.back_button.draw(surface)
+        surface.blit(
+            self.text,
+            (
+                surface.get_width() // 2 - self.text.get_width() // 2,
+                surface.get_height() // 2 - self.text.get_height() // 2,
+            ),
+        )
 
     def update(self, events, keys) -> None:
         for event in events:
